503b2849d4c5daa204c83b315fb16130dcf149ea
masayuki97 <162100110+masayuki97@users.noreply.github.com>
2024-06-11 00:47:22 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+  <?php
+
+  $score1=80;
+  $score2=60;
+  $score3=55;
+  $score4=40;
+  $score5=100;
+  $score6=25;
+  $score7=80;
+  $score8=95;
+  $score9=30;
+  $score10=60;
+  $sum=$score1 + $score2 + $score3 + $score4 +$score5+$score6+$score7+$score8+$score9+$score10;
+  $average=$sum /10;
+    
+  echo $sum;
+  echo '<br>';
+  echo $average;
+       
+  
+  
+  ?>
+  
+  
+</body>
+</html>
