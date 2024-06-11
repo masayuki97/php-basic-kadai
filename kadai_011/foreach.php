6e076deff74e16b65ee9e4f252f51b37a4ea91b1
masayuki97 <162100110+masayuki97@users.noreply.github.com>
2024-06-12 00:05:57 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+  
+  <?php
+  
+  $data=['名前'=>'玉ねぎ','値段'=>200,'産地'=>'北海道'];
+  foreach($data as $key =>$value){
+    echo "{$key}:{$value}".'<br>';
+    
+    
+  }
+  
+  ?>
+  
+</body>
+</html>
