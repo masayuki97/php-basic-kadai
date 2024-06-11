26ad5b0790834de1bad587e7933a314fb4d2ae4a
masayuki97 <162100110+masayuki97@users.noreply.github.com>
2024-06-11 22:36:15 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+  
+
+  
+  <?php
+  $data=['name'=>'onion','price'=>200,'weight'=>160];
+  print_r($data);
+  
+  ?>
+  
+  
+  
+</body>
+</html>
